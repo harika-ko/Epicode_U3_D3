31031995f8720340ce4a3f0a5bbafa1a267f1a90
Harika_Kommuri <harikakommuri35@gmail.com>
2022-06-22 20:06:37 +0200

    Changed Single Book to Class component & created state

--- d3-index/src/Components/SingleBook.jsx
+++ d3-index/src/Components/SingleBook.jsx
@@ -1,15 +1,26 @@
+import { Component } from "react"
 import { Card } from "react-bootstrap"
 
 
-const SingleBook = (props) => (
-    <div className="single-book">
-        <Card key={props.book.asin}>
-            <Card.Img className="single-image" src={props.book.img} style={{ width: "250px" }} />
-            <Card.Body>
-                <Card.Title>{props.book.title}</Card.Title>
-            </Card.Body>
-        </Card>
-    </div>
-);
+class SingleBook extends Component {
+
+    state = {
+        selected: false
+    }
+
+    render() {
+        return (
+
+            <div className="single-book">
+                <Card key={this.props.book.asin}>
+                    <Card.Img className="single-image" src={this.props.book.img} style={{ width: "250px" }} />
+                    <Card.Body>
+                        <Card.Title>{this.props.book.title}</Card.Title>
+                    </Card.Body>
+                </Card>
+            </div>
+        );
+    }
+}
 
 export default SingleBook
